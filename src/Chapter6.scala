243f375fcbf69ca2243788631ed66d60ab1fa617
kylepace <kylepace03@gmail.com>
2015-08-25 22:04:19 -0400

    Chapter 6 exercises.

--- src/Chapter6.scala
+++ src/Chapter6.scala
@@ -22,4 +22,34 @@ object Chapter6 {
         val (nextInt, newRng) = nonNegativeInt(rng)
         (1 / (nextInt + 1) toDouble, newRng)
     }
+    
+    def intDouble(rng: RNG): ((Int, Double), RNG) = {
+        val (nextInt, newRng) = rng.nextInt
+        val (nextDouble, dblRng) = double(newRng)
+        ((nextInt, nextDouble), dblRng)
+    }
+    
+    def doubleInt(rng: RNG): ((Double, Int), RNG) = {
+        val (pair, nextRng) = intDouble(rng)
+        ((pair._2, pair._1), nextRng)
+    }
+    
+    def double3(rng: RNG): ((Double, Double, Double), RNG) = {
+        val (db1, rng1) = double(rng)
+        val (db2, rng2) = double(rng1)
+        val (db3, rng3) = double(rng2)
+        ((db1, db2, db3), rng3)
+    }
+    
+    def ints(count: Int)(rng: RNG): (List[Int], RNG) = {
+        @annotation.tailrec    
+        def ints_h(count: Int, rng: RNG, acc: List[Int]): (List[Int], RNG) = {
+            if (count <= 0) (acc, rng)
+            else {
+                val (i, r) = rng.nextInt
+                ints_h(count -1, r, i :: acc)
+            }
+        }
+        ints_h(count, rng, List())
+    }
 }
