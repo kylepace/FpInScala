756b086ff43ec19d7a9ef5af75e6c359543e941c
kylepace <kylepace03@gmail.com>
2015-09-01 20:08:28 -0400

    map flatmap with states.

--- src/Chapter6.scala
+++ src/Chapter6.scala
@@ -14,12 +14,34 @@ case class SimpleRNG(seed: Long) extends RNG {
 object Chapter6 {
     type Rand[+A] = RNG => (A, RNG)
     
+    def unit[A](a: A): Rand[A] =
+        rng => (a, rng)
+    
     def map[A, B](s: Rand[A])(f: A => B): Rand[B] =
         rng => {
             val (a, rng2) = s(rng)
             (f(a), rng2)
         }
     
+    def mapViaFlatMap[A, B](s: Rand[A])(f: A => B): Rand[B] =
+        flatMap(s)(a => unit(f(a)))
+        
+    def flatMap[A, B](f: Rand[A])(g: A => Rand[B]): Rand[B] =
+        rng => {
+            val (a, rng2) = f(rng)
+            g(a)(rng2)
+        }
+    
+    def map2[A, B, C](ra: Rand[A], rb: Rand[B])(f: (A, B) => C): Rand[C] =
+        rng => {
+            val (a, rng2) = ra(rng)
+            val (b, rng3) = rb(rng2)
+            (f(a, b), rng3)
+        }
+        
+    def map2ViaFlatMap[A, B, C](ra: Rand[A], rb: Rand[B])(f: (A, B) => C): Rand[C] =
+        flatMap(ra)(a => map(rb)(b => f(a, b)))
+        
     def doubleWithMap(rng: RNG): Rand[Double] =
         map(nonNegativeInt)(i => (1 / (i + 1) toDouble))
     
