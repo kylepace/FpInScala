80666e24f129f6c8f228acdd94377fbd11803441
kylepace <kylepace03@gmail.com>
2016-01-02 23:46:24 -0500

    Chapter 12

--- src/Chapter12.scala
+++ src/Chapter12.scala
@@ -26,9 +26,45 @@ trait Applicative[F[_]] {
         apply(unit(f))(fa)
         
     def map2ByApply[A, B, C](fa: F[A], fb: F[B])(f: (A, B) => C): F[C] =
-        apply(map(fa)(f.curried))(fb)  
+        apply(map(fa)(f.curried))(fb)
+        
+    def map3[A, B, C, D](fa: F[A], fb: F[B], fc: F[C])(f: (A, B, C) => D): F[D] = {
+        apply(apply(apply(unit(f.curried))(fa))(fb))(fc)
+    }
+    
+    def map4[A, B, C, D, E](fa: F[A], fb: F[B], fc: F[C], fd: F[D])(f: (A, B, C, D) => E): F[E] = {
+        apply(apply(apply(apply(unit(f.curried))(fa))(fb))(fc))(fd)
+    }
 }
 
+sealed trait Validation[+E, +A]
+
+case class Failure[E](head: E, tail: Vector[E] = Vector()) extends Validation[E, Nothing]
+
+case class Success[A](a: A) extends Validation[Nothing, A]
+
 object Chapter12 {
+    def eitherMonad[E]: Monad[({type f[x] = Either[E, x]})#f] = new Monad[({type f[x] = Either[E, x]})#f] {
+        def unit[A](a: => A): Either[E, A] = Right(a)
+        def flatMap[A, B](ma: Either[E, A])(f: A => Either[E, B]): Either[E, B] =
+            ma.flatMap(f)
+        
+        def flatMapFromBook[A, B](ma: Either[E, A])(f: A => Either[E, B]): Either[E, B] = ma match {
+            case Right(a) => f(a)
+            case Left(e) => Left(e)
+        }
+    }
     
+    def validationApplicative[E]: Applicative[({type f[x] = Validation[E,x]})#f] = new Applicative[({type f[x] = Validation[E,x]})#f] {
+        def unit[A](a: => A): Validation[E, A] = Success(a)
+        def map2[A, B, C](fa: Validation[E, A], fb: Validation[E, B])(f: (A, B) => C): Validation[E, C] = (fa, fb) match {
+            case (Success(a), Success(b)) => Success(f(a, b))
+            case (Failure(h1, t1), Failure(h2, t2)) => Failure(h1, t1 ++ Vector(h2) ++ t2)
+            case (Success(_), Failure(h1, t1)) => Failure(h1, t1)
+            case (Failure(h1, t1), Success(_)) => Failure(h1, t1)
+//          can also be written in the shorthand with alias            
+//          case (Success(_), e@Failure(_, _)) => e
+//          case (e@Failure(_, _), Success(_)) => e
+        }
+    }
 }
